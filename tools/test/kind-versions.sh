2d7e20e71c8cd567fe9f7a0b27014c7c12922ffe
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-17 13:18:59 +0100

    Added comments

--- tools/test/kind-versions.sh
+++ tools/test/kind-versions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Helper script to automatically provision a Kind cluster with a version of Kubernetes,
 # install the helm chart and verify that works then repeat for all other versions.
-set -eu
+#
+set -u
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CHART_DIR=${CHART_DIR:-$SCRIPT_DIR/../../couchbase-operator}
 GENERATE_TEST=${GENERATE_TEST:-no}
@@ -11,12 +12,19 @@ SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.2}
 
 declare -a SUPPORTED_K8S_VERSIONS=('v1.17.11' 'v1.18.8' 'v1.19.1' 'v1.20.0' )
 
+# Remove all existing KIND clusters
 kind delete clusters --all
 
+# Run for each version and carry on, exit at end with relevant error code (if any)
+# The user can connect to any left running to debug with a version-specific kube-config file.
 exitCode=0
 for K8S_VERSION in "${SUPPORTED_K8S_VERSIONS[@]}"; do
     echo "Testing $K8S_VERSION"
+
+    # Create the test cluster
     kind create cluster --name "${K8S_VERSION}" --image kindest/node:"${K8S_VERSION}" --kubeconfig kubeconfig."${K8S_VERSION}"
+
+    # Optionally generate the chart
     if [[ "${GENERATE_TEST}" == "yes" ]]; then
         if [[ -f "${CHART_DIR}/generate.sh" ]]; then
             MIN_K8S_VERSION=$(echo "$K8S_VERSION"|cut -d'.' -f 2) /bin/bash "${CHART_DIR}/generate.sh"
@@ -25,10 +33,13 @@ for K8S_VERSION in "${SUPPORTED_K8S_VERSIONS[@]}"; do
         fi
     fi
 
+    # Load any local images we may want into the cluster by default (if they exist locally)
     [[ -n $(docker images -q "${OPERATOR_IMAGE}" 2> /dev/null) ]] &&    kind load docker-image "${OPERATOR_IMAGE}" --name "${K8S_VERSION}"
     [[ -n $(docker images -q "${DAC_IMAGE}" 2> /dev/null) ]] &&         kind load docker-image "${DAC_IMAGE}" --name "${K8S_VERSION}"
     [[ -n $(docker images -q "${SERVER_IMAGE}" 2> /dev/null) ]] &&      kind load docker-image "${SERVER_IMAGE}" --name "${K8S_VERSION}"
 
+    # Run a helm install to confirm
+    # Call this script with `-f ./image-values.yaml` for example to pass in overrides
     if helm upgrade --kubeconfig kubeconfig."${K8S_VERSION}" --install --debug --wait couchbase "${CHART_DIR}" "$@" ; then
         echo "PASSED: $K8S_VERSION"
         kind delete cluster --name "${K8S_VERSION}"
