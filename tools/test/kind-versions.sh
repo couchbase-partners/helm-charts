96a1752bc9d28c21fbb00ba7484e5d66cd5a0319
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-18 11:55:32 +0100

    Updated test script to handle multi-node cluster and check CB server is running post install

--- tools/test/kind-versions.sh
+++ tools/test/kind-versions.sh
@@ -26,16 +26,31 @@ fi
 # Remove all existing KIND clusters
 kind delete clusters --all
 
+CLUSTER_CONFIG=$(mktemp)
+cat << EOF > "${CLUSTER_CONFIG}"
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+featureGates:
+ EphemeralContainers: true
+nodes:
+- role: control-plane
+EOF
+
+for _ in $(seq "$SERVER_COUNT"); do
+    cat << EOF >> "${CLUSTER_CONFIG}"
+- role: worker
+EOF
+done
+
 # Run for each version and carry on, exit at end with relevant error code (if any)
 # The user can connect to any left running to debug with a version-specific kube-config file.
 exitCode=0
 
-# Initially we spin up the cluster and confirm helm installs
 for K8S_VERSION in "${SUPPORTED_K8S_VERSIONS[@]}"; do
     echo "Testing Helm install on $K8S_VERSION"
 
     # Create the test cluster
-    kind create cluster --name "${K8S_VERSION}" --image kindest/node:"${K8S_VERSION}" --kubeconfig kubeconfig."${K8S_VERSION}"
+    kind create cluster --name "${K8S_VERSION}" --config "${CLUSTER_CONFIG}" --image kindest/node:"${K8S_VERSION}" --kubeconfig kubeconfig."${K8S_VERSION}"
 
     # Load any local images we may want into the cluster by default (if they exist locally)
     [[ -n $(docker images -q "${OPERATOR_IMAGE}" 2> /dev/null) ]] &&    kind load docker-image "${OPERATOR_IMAGE}" --name "${K8S_VERSION}"
@@ -44,40 +59,36 @@ for K8S_VERSION in "${SUPPORTED_K8S_VERSIONS[@]}"; do
 
     # Run a helm install to confirm
     # Call this script with `-f ./image-values.yaml` for example to pass in overrides
-    if helm upgrade --kubeconfig kubeconfig."${K8S_VERSION}" --install --debug --wait couchbase "${CHART_DIR}" "$@" ; then
-        echo "PASSED: $K8S_VERSION"
-    else 
-        echo "FAILED: $K8S_VERSION"
+    if helm upgrade --kubeconfig kubeconfig."${K8S_VERSION}" --install --debug --wait couchbase "${CHART_DIR}" "$@" &> helm-install-"${K8S_VERSION}".log ; then
+        echo "Verifying Helm install on $K8S_VERSION"
+
+        # Wait for deployment to complete
+        attempts=1
+        echo "Waiting for CB to start up..."
+        until [[ $(kubectl --kubeconfig kubeconfig."${K8S_VERSION}" get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
+            if [[ $attempts -ge $MAX_ATTEMPTS ]]; then
+                echo "FAILED: Couchbase cluster not ready after $attempts attempts"
+                kubectl --kubeconfig kubeconfig."${K8S_VERSION}" get pods --field-selector=status.phase=Running --selector='app=couchbase'
+                exitCode=1
+                break
+            else
+                attempts=$((attempts+1))
+            fi
+            echo -n '.'
+            sleep 10
+        done
+        echo "Exited loop"
+
+        if [[ $attempts -lt $MAX_ATTEMPTS ]]; then
+            echo "PASSED: Couchbase Cluster ready for ${K8S_VERSION}"
+            kind delete cluster --name "${K8S_VERSION}"
+            rm -f kubeconfig."${K8S_VERSION}"
+        fi
+    else
+        echo "FAILED: Helm install for $K8S_VERSION"
         exitCode=1
     fi
 done
 
-# Now we go back to verify we have a Couchbase Cluster as well on each kind cluster
-for K8S_VERSION in "${SUPPORTED_K8S_VERSIONS[@]}"; do
-    echo "Verifying Helm install on $K8S_VERSION"
-
-    # Wait for deployment to complete
-    attempts=1
-    echo "Waiting for CB to start up..."
-    until [[ $(kubectl --kubeconfig kubeconfig."${K8S_VERSION}" get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
-        if [[ $attempts -ge $MAX_ATTEMPTS ]]; then
-            echo "FAILED: Couchbase cluster not ready after $attempts attempts"
-            kubectl --kubeconfig kubeconfig."${K8S_VERSION}" get pods --field-selector=status.phase=Running --selector='app=couchbase'
-            exitCode=1
-            break
-        else
-            attempts=$((attempts+1))
-        fi
-        echo -n '.'
-        sleep 10
-    done
-    echo 
-
-    if [[ $attempts -lt $MAX_ATTEMPTS ]]; then
-        echo "PASSED: Couchbase Cluster ready"
-        kind delete cluster --name "${K8S_VERSION}"
-        rm -f kubeconfig."${K8S_VERSION}"
-    fi
-done
-
+rm -f "${CLUSTER_CONFIG}"
 exit $exitCode
